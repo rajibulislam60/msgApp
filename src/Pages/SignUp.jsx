0c057a455f4304853fa124700aa6ac4f434dbff2
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-09-11 21:33:02 +0600

    working signup page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignUpImage from '../assets/signUp.jpg'
+import Button from '../components/Button';
 
 const SignUp = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="ml-[69px]">
-        <h1 className="text-[34px] font-bold text-[#11175D]">
+      <div className="w-2/4 ml-[70px]">
+        <h1 className="text-[34px] font-bold text-[#11175D] uppercase">
           Get started with easily register
         </h1>
         <p className="text-[22px] font-normal text-[#000000] mt-3">
@@ -33,6 +34,16 @@ const SignUp = () => {
         </div>
         <div className="w-[368px] h-[80px] mt-[61px] relative">
           <label className="text-sm font-semibold text-secondary absolute top-[-10px] left-[50px] bg-white px-4">
+            Mobile Number
+          </label>
+          <input
+            className=" w-full h-full border border-secondary/50 rounded-lg pl-[62px]"
+            type="number"
+            placeholder="Enter Mobile Number"
+          />
+        </div>
+        <div className="w-[368px] h-[80px] mt-[61px] relative">
+          <label className="text-sm font-semibold text-secondary absolute top-[-10px] left-[50px] bg-white px-4">
             Password
           </label>
           <input
@@ -40,6 +51,9 @@ const SignUp = () => {
             type="mail"
             placeholder="Enter Password"
           />
+
+        {/* ----------------button area----------------------- */}
+        <Button/>
         </div>
       </div>
       <div className="w-2/4 h-full">
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div className="text-[18px] font-medium leading-[22px] bg-[#89EAFE] px-3 py-2">
+      test
+    </div>
+  );
+};
+
+export default Button;
